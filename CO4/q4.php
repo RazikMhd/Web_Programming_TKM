2a496816cb0d6ba6d6c6fcfcdc269c2583c30d12
Razik <rasikmhd10@gmail.com>
2021-02-25 00:06:52 +0530

    CO4 Complete

---
+++ CO4/q4.php
@@ -0,0 +1,13 @@
+<?php 
+
+$players = array("Sachin","Ganguly","Dravid","Kohli","Kumble");
+
+echo "<table border='1'>";
+echo "<tr><th>Players</th></tr>";
+for($i=0;$i<count($players);$i++)
+{
+    echo "<tr><td>".$players[$i]."</td></tr>";
+}
+echo "</table>";
+
+?>
